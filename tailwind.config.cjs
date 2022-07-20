62391e6a196fd92be83ff75a3e1afb011e348dae
valderyjr <valderyjunior2002@gmail.com>
2022-07-20 15:58:51 -0300

    customizing the gray colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          800: "#171717",
+          900: "#080808",
+        },
+      },
+    },
   },
   plugins: [],
 };
