64145b9863b632ba19af710cd680bf60b657bffd
valderyjr <valderyjunior2002@gmail.com>
2022-07-20 18:01:13 -0300

    creating service to get jokes from api

---
+++ src/services/httpService.ts
@@ -0,0 +1,20 @@
+import { httpService } from "../config/http";
+
+interface OneJoke {
+  categories: string[];
+  created_at: string;
+  icon_url: string;
+  id: string;
+  updated_at: string;
+  url: string;
+  value: string;
+}
+
+export const getARandomJoke = async (): Promise<OneJoke | null> => {
+  try {
+    const { data } = await httpService.get("/jokes/random");
+    return data as OneJoke;
+  } catch (error) {
+    return null;
+  }
+};
