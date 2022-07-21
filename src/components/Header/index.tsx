a703e683f0c6ac6da69c1df44c7a0527aa70b821
valderyjr <valderyjunior2002@gmail.com>
2022-07-21 01:50:58 -0300

    fix header: change <a> tags to <Link>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logoChuck from "../../assets/chucknorris.png";
+import { Link } from "react-router-dom";
 
 interface ILinksHeaderNav {
   name: string;
@@ -22,18 +23,18 @@ const Header = () => {
   ];
   return (
     <header className="w-full bg-gray-800 h-32 flex flex-col justify-center gap-4 px-4 md:px-8 md:flex-row md:justify-between md:items-center">
-      <a href="/" className="h-1/2 w-fit mx-auto md:h-full md:mx-0">
+      <Link to="/" className="h-1/2 w-fit mx-auto md:h-full md:mx-0">
         <img
           src={logoChuck}
           alt="Logo do Chuck Norris."
           className="w-full h-full"
         />
-      </a>
+      </Link>
       <nav>
         <ul className="flex gap-5 justify-center">
           {linksNav.map((link) => (
             <li className="font-semibold hover:text-gray-50" key={link.slug}>
-              <a href={link.to}>{link.name}</a>
+              <Link to={link.to}>{link.name}</Link>
             </li>
           ))}
         </ul>
