1e0520233597546f41fa60e65f0f3960555c8468
valderyjr <valderyjunior2002@gmail.com>
2022-07-20 21:54:53 -0300

    creating input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,19 @@
+import React, { InputHTMLAttributes } from "react";
+
+interface IInput extends InputHTMLAttributes<HTMLInputElement> {
+  value: string;
+  handleInput: (value: string) => void;
+}
+
+const Input = ({ value, handleInput, ...rest }: IInput) => {
+  return (
+    <input
+      value={value}
+      onChange={({ target }) => handleInput(target.value)}
+      {...rest}
+      className="text-gray-200 font-semibold pl-2 py-2 bg-transparent border-b border-gray-50 outline-none"
+    />
+  );
+};
+
+export default Input;
