b4d14b48ab94c2d7e0bbf2d69e552bc76a8c395c
valderyjr <valderyjunior2002@gmail.com>
2022-07-22 14:25:14 -0300

    fix the myFavorites page to use ternary operator

--- src/pages/MyFavorites/index.tsx
+++ src/pages/MyFavorites/index.tsx
@@ -1,30 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CardJoke from "../../components/CardJoke";
 import PageTitle from "../../components/PageTitle";
 import { useAppContext } from "../../hooks/useAppContext";
 import AppLayout from "../../layouts/AppLayout";
 
 const MyFavorites = () => {
-  const [error, setError] = useState(false);
   const { myJokes } = useAppContext();
-  useEffect(() => {
-    myJokes.length > 0 ? setError(false) : setError(true);
-  }, [myJokes]);
   return (
     <AppLayout>
       <PageTitle title="Estas são suas piadas favoritas" />
-      {error && (
-        <div>
-          <p className="text-2xl mt-8 text-red-600 font-bold text-center">
-            Infelizmente você ainda não escolheu nenhuma piada.
-          </p>
-          <p className="text-xl mt-2 text-red-600 font-bold text-center">
-            Chuck Norris disapproved!
-          </p>
-        </div>
-      )}
       <div className="mt-8 grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:gap-x-4 gap-y-8 p-2 justify-items-center">
-        {myJokes.length > 0 &&
+        {myJokes.length > 0 ? (
           myJokes.map((joke) => (
             <CardJoke
               key={joke.id}
@@ -32,7 +18,17 @@ const MyFavorites = () => {
               createdAt={joke.createdAt}
               text={joke.text}
             />
-          ))}
+          ))
+        ) : (
+          <div className="flex flex-col sm:col-span-2 md:col-span-3 gap-8">
+            <h4 className="text-xl sm:text-2xl text-red-600 font-bold text-center">
+              Infelizmente você ainda não escolheu nenhuma piada.
+            </h4>
+            <h6 className="block text-base sm:text-xl text-red-600 font-bold text-center">
+              Chuck Norris disapproved!
+            </h6>
+          </div>
+        )}
       </div>
     </AppLayout>
   );
